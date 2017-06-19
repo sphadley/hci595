30738b2047aa1b107d47f5faf4789bc0706901ac
Steven Hadley <sphadley@gmail.com>
2017-06-19 18:50:06 -0500

    update data model

---
+++ js/gardens.js
@@ -0,0 +1,74 @@
+gardens = [
+    {
+        Name: "Garden1",
+        GardenInfo:"Contact Bob",
+        Latlng : [0,0],
+        Marker : 'm1.png',
+        Puzzles:
+        [
+            {
+                Question: "what?",
+                Answer: "Blue Sky",
+                Badge: "Badge1-1.png"
+            }
+        ],
+        Gnome : {
+            Name: "Gnome1",
+            Image: "Gnome1.png"
+        }
+    },
+    {
+        Name: "Garden2",
+        GardenInfo:"Contact Jenny",
+        Latlng : [0,0],
+        Marker : 'm2.png',
+        Puzzles:
+        [
+            {
+                Question: "what?",
+                Answer: "Blue Sky",
+                Badge: "Badge2-1.png"
+            }
+        ],
+        Gnome : {
+            Name: "Gnome2",
+            Image: "Gnome2.png"
+        }
+    },
+    {
+        Name: "Garden3",
+        GardenInfo:"Contact Tanya",
+        Latlng : [0,0],
+        Marker : 'm3.png',
+        Puzzles:
+        [
+            {
+                Question: "what?",
+                Answer: "Blue Sky",
+                Badge: "Badge3-1.png"
+            }
+        ],
+        Gnome : {
+            Name: "Gnome3",
+            Image: "Gnome3.png"
+        }
+    },
+    {
+        Name: "Garden4",
+        GardenInfo:"Contact Bob",
+        Latlng : [0,0],
+        Marker : 'm4.png',
+        Puzzles:
+        [
+            {
+                Question: "what?",
+                Answer: "Blue Sky",
+                Badge: "Badge4-1.png"
+            }
+        ],
+        Gnome : {
+            Name: "Gnome4",
+            Image: "Gnome4.png"
+        }
+    }
+]
